acd174ac05f0b88c1531bc5ed3dccd7462f48e4a
Diogenes <diogenes.henrique.br@gmail.com>
2021-04-15 01:37:57 -0300

    Add navbar dropdown Menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+window.jQuery = $; window.$ = $;    
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import '../stylesheets/application'
-import 'materialize-css/dist/js/materialize'
+import "materialize-css/dist/js/materialize"
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).on('turbolinks:load', function() {
+     $(".dropdown-trigger").dropdown();
+     $('.sidenav').sidenav();
+  });
