4b973dad5e4df32f7fdec9b482e66b82eac33814
Diogenes <diogenes.henrique.br@gmail.com>
2021-04-19 23:06:44 -0300

    DRY your code - extract redundancies

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,12 @@
 class StudentsController < ApplicationController
+    before_action :set_student, only: [:show, :edit, :update] 
+
     def index
         @students = Student.all
     end
 
     def show
-        @student = Student.find(params[:id])
+        
     end
 
     def new
@@ -15,13 +17,31 @@ class StudentsController < ApplicationController
         @student = Student.new(student_params)
         if @student.save
             flash[:notice] = "You have successfully signed up"
-            redirect_to root_path
+            redirect_to @student
         else
             render 'new'
         end
     end
 
+    def edit
+        
+    end
+
+    def update
+        
+        if @student.update(student_params)
+            flash[:notice] = "you have successfully updated your profile"
+            redirect_to @student 
+        else
+            render 'edit'
+        end
+    end
+
     private
+
+    def set_student
+        @student = Student.find(params[:id])
+    end
 
     def student_params
         params.require(:student).permit(:name, :email)
